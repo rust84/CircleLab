3dbc97c658c35ccc9c871edcfed879161f55cd1f
Russell Hall <owner@russellhall.co.uk>
2024-01-05 00:53:33 +0000

    assume bucket role

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region = var.region
 
-  # assume_role {
-  #   role_arn = ""
-  # }
+  assume_role {
+    role_arn = "arn:aws:iam::446168904202:role/terraform-bucket-access"
+  }
 
   default_tags {
     tags = {
