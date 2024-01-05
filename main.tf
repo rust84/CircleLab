0e8ee7eeb38782001f1043395f06b79c3f3313bf
Russell Hall <owner@russellhall.co.uk>
2024-01-05 00:31:27 +0000

    enable bucket controls

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ resource "aws_s3_bucket" "app" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "control" {
+  bucket = aws_s3_bucket.app.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
 resource "aws_s3_bucket_acl" "bucket" {
   bucket = aws_s3_bucket.app.id
