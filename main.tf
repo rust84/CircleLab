e3aa63729ced5bbca49cc92f95d17df47267a842
Russell Hall <owner@russellhall.co.uk>
2024-01-08 11:50:16 +0000

    enable public access block

--- main.tf
+++ main.tf
@@ -9,14 +9,18 @@ resource "aws_s3_bucket" "app" {
   force_destroy = true
 }
 
-# resource "aws_s3_bucket_ownership_controls" "control" {
-#   bucket = aws_s3_bucket.app.id
-#   rule {
-#     object_ownership = "BucketOwnerPreferred"
-#   }
-# }
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.app.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
 
-# resource "aws_s3_bucket_acl" "bucket" {
-#   bucket = aws_s3_bucket.app.id
-#   acl    = "private"
-# }
+resource "aws_s3_bucket_public_access_block" "public-access-block" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
